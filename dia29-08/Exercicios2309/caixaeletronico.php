42b80a48f6606667b3cc1f67f6097ce1d9c9496e
MateusAugustoFerreira <mateusaugustoferreira@gmail.com>
2022-09-26 17:53:48 -0300

    2609

--- dia29-08/Exercicios2309/caixaeletronico.php
+++ dia29-08/Exercicios2309/caixaeletronico.php
@@ -1,12 +1,12 @@
 <h1><em>Consulta de saldo:</em></h1>
 
-<form action="caixaeletronico.php" method="$_GET">
+<form action="consulta.php" method="$_GET">
     <button type="text">Consultar</button>
 </form>
 
 <h1>Retirar valor x: </h1>
 
-<form action="caixaeletronico.php" method="$_GET">
+<form action="retirar.php" method="$_GET">
     <button type="text">Retirar</button>
 </form>
 
---
+++ dia29-08/Exercicios2309/consulta.php
@@ -0,0 +1,11 @@
+<h1>Consulta de saldo!</h1>
+
+<hr>
+
+<h3><em>O valor de seu saldo é de:</em></h3>
+
+<h2>Retirar valor x: </h2>
+
+<form action="retirar.php" method="$_GET">
+    <button type="text">Retirar</button>
+</form>
